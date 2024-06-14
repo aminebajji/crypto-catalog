1d26e6c3e5cf63a90031a935438a996f0e077545
AmineBAJJI <aminebajji18@gmail.com>
2024-06-14 15:57:56 +0100

    trying to fix NextUI prod

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["coin-images.coingecko.com", "s3.coinmarketcap.com"],
-  },
-  reactStrictMode: true,
-};
-
-export default nextConfig;
